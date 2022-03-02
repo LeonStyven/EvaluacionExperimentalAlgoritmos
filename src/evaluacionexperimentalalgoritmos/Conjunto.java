cf7df4aacfa4ca09964f53fa8291a53837292617
Leon Styven <j.stca03@gmail.com>
2022-03-01 22:35:55 -0500

    Implementacion de metodos

--- src/evaluacionexperimentalalgoritmos/Conjunto.java
+++ src/evaluacionexperimentalalgoritmos/Conjunto.java
@@ -21,50 +21,76 @@ public class Conjunto
        bolsa = null;
        n = 0;
     }
-    
+    /*
     //Constructor con bolsa hecha
     public Conjunto(Bag<Object> list)
     {
         this.bolsa = list;
         n = list.size();
     }
+    */
     
-    //Corregir add, verificar No iguales
+    /*
+    Añade un nuevo elemento de cualquier tipo despues de verificar que no este en la lista
+    */
     public void Add(Object elemento)
     {
-        bolsa.add(elemento);
-        n++;
+        //Verificar que no este el elemento
+        if(!In(elemento)){
+            bolsa.add(elemento);
+            n++;
+        }
     }
-    
+    /*
+    Remueve el elemento que le indiquemos
+    Se le pasa el elemento que queremos eliminar
+    Al final la bolsa resultante es inversa a la original sin el elemnto
+    En caso de no encontrarlo entonces no hace nada
+    */
     public void Remove(Object elemento)
     {
-        for(int i = 0; i<=n ; i++)
-        {
-            
+        Bag<Object> bolsaAux = null; //Almacena elementos que vamos sacando
+        if(In(elemento)){
+            for(int i = 0; i<=n ; i++)
+            {
+                Object valor = bolsa.remove();
+                //Comparar elementos, si no es igual entonces no se guarda
+                if (elemento != valor) bolsaAux.add(valor);
+            }
+            bolsa = bolsaAux;
+            n--;
         }
+        
     }
     
+    /*Ver si un elemento esta en el conjunto
+    si el retorno es False entonces no lo encontro
+    La lista resultante queda al reves de como ingreso
+    */
     
-    public int In(Object elemento)
+    public Boolean In(Object elemento)
     {
-        Bag<Object> bolsa2;
-        int indice = -1;
-        for(int i = 0; i<=n ; i++)
+        Bag<Object> bolsaAux = null; //Almacena elementos que vamos sacando
+        Boolean existe = false;
+        
+        //Vamos sacando elementos
+        for(int i = 0; i<n ; i++)
         {
             Object valor = bolsa.remove();
-            
-            
-            if (elemento == valor)
-            {
-                indice++;
-                
-                
-            }
+            //Comparar elementos
+            if (elemento == valor) existe=true;
+            bolsaAux.add(valor);
         }
-        return indice;
+        bolsa = bolsaAux;
+        return existe;
     }
-            
-    
-    
-            
+    //Saber el tamaño del conjunto
+    public int Size(){
+        return n;
+    }
+    //Saber si el conjunto esta vacio
+    public Boolean isEmpty(){
+        return bolsa == null;
+    }
+       
 }
