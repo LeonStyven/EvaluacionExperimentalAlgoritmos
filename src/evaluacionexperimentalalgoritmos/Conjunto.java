d2371c796f14953cbc821eea91ed616826e442de
JulianG91012 <juliangomez91012@gmail.com>
2022-03-01 21:55:51 -0500

    Creacion de metodos solicitados

--- src/evaluacionexperimentalalgoritmos/Conjunto.java
+++ src/evaluacionexperimentalalgoritmos/Conjunto.java
@@ -8,6 +8,63 @@ package evaluacionexperimentalalgoritmos;
  *
  * @author jstca
  */
-public class Conjunto {
+public class Conjunto 
+{
+    private Bag<Object> bolsa;
+    //add, remove, in, size, isEmpty
+    //Hash del equipo
+    private int n;
     
+    //Se declara el conjunto vacío
+    public Conjunto()
+    {    
+       bolsa = null;
+       n = 0;
+    }
+    
+    //Constructor con bolsa hecha
+    public Conjunto(Bag<Object> list)
+    {
+        this.bolsa = list;
+        n = list.size();
+    }
+    
+    //Corregir add, verificar No iguales
+    public void Add(Object elemento)
+    {
+        bolsa.add(elemento);
+        n++;
+    }
+    
+    public void Remove(Object elemento)
+    {
+        for(int i = 0; i<=n ; i++)
+        {
+            
+        }
+    }
+    
+    
+    public int In(Object elemento)
+    {
+        Bag<Object> bolsa2;
+        int indice = -1;
+        for(int i = 0; i<=n ; i++)
+        {
+            Object valor = bolsa.remove();
+            
+            
+            if (elemento == valor)
+            {
+                indice++;
+                
+                
+            }
+        }
+        return indice;
+    }
+            
+    
+    
+            
 }
